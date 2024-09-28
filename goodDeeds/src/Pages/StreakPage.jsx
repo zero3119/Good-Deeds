0a7ea326361c6403e6a60c794a4ef34d5ffd4f05
zero3119 <127268380+zero3119@users.noreply.github.com>
2024-09-28 13:11:57 -0600

    Header

---
+++ goodDeeds/src/Pages/StreakPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../header/Header";
+
+function StreakPage() {
+    return (
+        
+    <>
+    <Header/>
+        <div className="bg-[#ace9b3] w-screen h-screen flex">
+        </div>
+    </>
+        
+    );
+}
+
+export default StreakPage;
